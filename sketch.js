88c7221c2a66a024f6fc2754b2ef0935d40822f0
thealinealmeida <68761501+thealinealmeida@users.noreply.github.com>
2023-10-10 18:08:21 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
+
+function preload(){
+ 
+}
+
+function setup(){
+  
+}
+
+function draw(){
+  
+}
+
+function spawnDoors() {
+
+}
+
